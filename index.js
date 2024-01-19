a80f1b48b02973c52fbf5544647e0a62293d488e
evrajireddy <60527703+evrajireddy@users.noreply.github.com>
2024-01-18 22:47:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 function sumReducer1(arr){
-arr.reduce((sum, number)=> {
+  
+  //reduce function accepts 2 parameters, then invokes the callback for each item of the array, updating the accumulator at each step. 
+  //first parameter is the callback function that updates the accumulator value, 
+  //second parameter is the initial value of the accumulator. 
+  
+  arr.reduce((sum, number)=> {
   const updatedSum = sum + number;
   return updatedSum;
 }, 0);
@@ -10,7 +15,10 @@ console.log(sumReducer1([1, 3, 5, 7]));
 
 
 
-function sumReducer2(arr){
+function sumReducer2(arr){ 
+  
+  //second parameter omitted so that accumulator value is initialized with the first item of the array, and the iteration starts from the second item.
+  
 arr.reduce((sum, number, index)=>() {
   return sum + number;
 });
